172a0c376008e7bf12378b17e5d101ceb66d9b99
Caio <c410.f3r@gmail.com>
2020-11-29 16:08:24 -0300

    Fix fuzz targets

--- fuzz/parsers.rs
+++ fuzz/parsers.rs
@@ -4,27 +4,26 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use oapth::{Config, parse_cfg, parse_migration};
+use oapth::{parse_cfg, parse_migration, Config};
 use std::path::Path;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = parse_cfg(data, &Path::new("."));
-    let _ = parse_migration(data);
+  let _ = parse_cfg(data, &Path::new("."));
+  let _ = parse_migration(data);
 
-    let s = if let Ok(rslt) = core::str::from_utf8(data) {
-        rslt
-    }
-    else {
-        return;
-    };
+  let s = if let Ok(rslt) = core::str::from_utf8(data) {
+    rslt
+  } else {
+    return;
+  };
 
-    let c = Config::with_url(s);
-    let _ = c.database();
-    let _ = c.full_host();
-    let _ = c.host();
-    let _ = c.name();
-    let _ = c.password();
-    let _ = c.port();
-    let _ = c.url();
-    let _ = c.user();
+  let c = Config::with_url(s);
+  let _ = c.database();
+  let _ = c.full_host();
+  let _ = c.host();
+  let _ = c.name();
+  let _ = c.password();
+  let _ = c.port();
+  let _ = c.url();
+  let _ = c.user();
 });
