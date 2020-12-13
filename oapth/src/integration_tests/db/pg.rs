fb77ed392a423f95254a58bb1b46da0ac5baf8e4
zacck <coderv63@gmail.com>
2020-12-13 16:04:48 +0300

    Count enums as types

--- oapth/src/integration_tests/db/pg.rs
+++ oapth/src/integration_tests/db/pg.rs
@@ -11,7 +11,6 @@ pub async fn clean_drops_all_objs<B>(
   crate::integration_tests::create_foo_table(c, "bar.").await;
   assert_eq!(c.back_end.tables("bar").await.unwrap().len(), 1);
   c.back_end.execute("CREATE VIEW view AS SELECT * FROM foo WHERE id = 1;").await.unwrap();
-  c.back_end.execute("CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy');").await.unwrap();
   c.back_end.execute("CREATE DOMAIN integer0 AS INTEGER CONSTRAINT must_be_greater_than_or_equal_to_zero_chk CHECK(VALUE >= 0);").await.unwrap();
   c.back_end.execute("CREATE FUNCTION time_subtype_diff(x time, y time) RETURNS float8 AS 'SELECT EXTRACT(EPOCH FROM (x - y))' LANGUAGE sql STRICT IMMUTABLE").await.unwrap();
   c.back_end.execute("CREATE PROCEDURE something() LANGUAGE SQL AS $$ $$").await.unwrap();
@@ -26,7 +25,6 @@ pub async fn clean_drops_all_objs<B>(
   assert_eq!(crate::fixed_sql_commands::pg::procedures(&mut c.back_end, "public").await.unwrap().len(), 1);
   assert_eq!(crate::fixed_sql_commands::pg::types(&mut c.back_end, "public").await.unwrap().len(), 1);
   assert_eq!(crate::fixed_sql_commands::pg::sequences(&mut c.back_end, "public").await.unwrap().len(), 1);
-  assert_eq!(crate::fixed_sql_commands::pg::enums(&mut c.back_end, "public").await.unwrap().len(), 1);
 
   c.clean().await.unwrap();
 
